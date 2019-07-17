fc5ce4dcb71dba9b6d4b2675c5cdcdac64d24c99
jorgej2 <jorgej2@nationwide.com>
2019-07-17 13:53:03 -0400

    Retrieve docusign configurations from web service.

---
+++ src/app/products/docusignConfiguration.ts
@@ -0,0 +1,13 @@
+export interface IDocusignConfiguration {
+    docuSignSubscriptionFiles: String;
+    fileSubscriptionCases: String;
+    fileTransferDirectory: String;
+    fileTransferId: String;
+    fileTransferMethod: String;
+    seqId: String;
+    subscriptionBeginDate: String;
+    subscriptionEndDate: String;
+    subscriptionName: String;
+    subscriptionStatus: String;
+
+}
