722a0854a98c007cbc658407791a7850109aa503
jorgej2 <jorgej2@nationwide.com>
2019-07-25 16:51:14 -0400

    Removed commented out code.

--- src/app/products/products-detail.component.ts
+++ src/app/products/products-detail.component.ts
@@ -30,60 +30,6 @@ export class ProductsDetailComponent implements OnInit {
       },
       error => this.errorMessage = <any>error  // casting operator
     ); 
-
-    // this.pageTitle +=`: ${id}`;
-
-
-    // this.product = {
-    //   'productId': id,
-    //   'productName': 'Schools First',
-    //   'productCode': 'GDN-0011',
-    //   'releaseDate': 'Mar 19, 2016',
-    //   'description': 'Leaf rake with 48 inch wooden handle.',
-    //   'price': "19.95",
-    //   'starRating': 3.2,
-    //   'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/2615/Anonymous_leaf_Rake.png',
-
-    //   'seqId': '',
-    //   'subscriptionName': 'Schools First',
-    //   'subscriptionBeginDate': 'Jan 1, 2017',
-    //   'subscriptionEndDate': 'N/A',
-    //   'subscriptionStatus': 'Active',
-    //   'fileTransferMethod': '',
-    //   'fileTransferId': '',
-    //   'fileTransferDirectory': '',
-    //   'fileSubscriptionCases': '',
-    //   'docuSignSubscriptionFiles': '',
-    
-
-    //   'cases': 'SCHOOLSFIRST FEDERAL',
-
-    //   'censusFile': 'Yes',
-    //   'censusFileBeginDate': 'Jan 1, 2017',
-    //   'censusFileEndDate': 'N/A',
-    //   'censusFileCreateWhenEmpty': 'Yes',
-    //   'censusFileInternalEmailNotificationAddress': '',
-
-    //   'sraFile': 'Yes',
-    //   'sraFileBeginDate': 'Jan 1, 2017',
-    //   'sraFileEndDate':  'N/A',
-    //   'sraFileCreateWhenEmpty': 'Yes',
-    //   'sraFileInternalEmailNotificationAddress': '',
-
-    //   'enrollmentFile': 'Yes',
-    //   'enrollmentFileBeginDate': 'Jan 1, 2017',
-    //   'enrollmentFileEndDate': 'N/A',
-    //   'enrollmentFileCreateWhenEmpty': 'Yes',
-    //   'enrollmentFileInternalEmailNotificationAddress':  '',
-
-    //   'pdfFile': 'Yes',
-    //   'pdfFileBeginDate': 'Mar 18, 2018',
-    //   'pdfFileEndDate': 'N/A',
-
-    //   'transferMethod':'EB2B',
-    //   'transferId': '/devl/rptest1/eb2b',
-    //   'transferDirectory': '/transport/in/PENSIONS/KEYNOTE_OUT',
-    // }
   }
 
   onBack(): void {
